80d5f35d7aa96666a6cf38341cf947a995303daf
Alexander Grashenko <grashenko1410@mail.ru>
2019-11-24 17:12:52 +0300

    com

--- binaryclassification.py
+++ binaryclassification.py
@@ -6,14 +6,18 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import GradientBoostingClassifier
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 data_train = pd.read_csv('train.csv', delimiter=',')
-data_test = pd.read_csv('train.csv', delimiter=',')
+data_test = pd.read_csv('test.csv', delimiter=',')
 
-data_train =data_train.dropna()
+data_train = data_train.dropna()
 data_test = data_test.dropna()
 
-print(data_train.columns[data_train.isnull().values.any()].tolist())
+train, validation = train_test_split(data_train, test_size=0.2)
+
+print(train.columns[train.isnull().values.any()].tolist())
 
 arr_name = []
 arr_train = []
@@ -25,16 +29,16 @@ col_y = 'Survived'
 
 def test_classifier(classifier, classifier_name):
     # обучаем классификатор
-    classifier.fit(data_train[cols_x], data_train[col_y])
+    classifier.fit(train[cols_x], train[col_y])
 
     # проверяем классификатор
-    y_train = classifier.predict(data_train[cols_x])
+    y_train = classifier.predict(train[cols_x])
     
     # определяем точность
-    y_train_acc = accuracy_score(data_train[col_y], y_train)
+    y_train_acc = accuracy_score(train[col_y], y_train)
     # для валидационной выборки
-    y_val = classifier.predict(data_test[cols_x])
-    y_val_acc = accuracy_score(data_test[col_y], y_val)
+    y_val = classifier.predict(validation[cols_x])
+    y_val_acc = accuracy_score(validation[col_y], y_val)
 
     # сохранение информации в массивы
     arr_name.append(classifier_name)
@@ -53,11 +57,37 @@ def test_classifier(classifier, classifier_name):
 
 
 classifier = test_classifier(KNeighborsClassifier(), 'KNN')
+
 classifier = test_classifier(LogisticRegression(), 'LR')
+
 classifier = test_classifier(SVC(), 'SVM')
+
 classifier = test_classifier(DecisionTreeClassifier(), 'Tree')
 for col, i in zip(cols_x, classifier.feature_importances_):
     print(col, ": ", i)
+
 classifier = test_classifier(GradientBoostingClassifier(), 'GB')
 for col, i in zip(cols_x, classifier.feature_importances_):
     print(col, ": ", i)
+
+x = range(len(arr_train))
+plt.plot(x, arr_train)
+plt.plot(x, arr_val)
+plt.xticks(x, arr_name)
+plt.ylabel('Точность алгоритма')
+plt.legend(['обучение', 'валидация'], loc='lower left')
+plt.show(block = False)
+
+
+cols_x = ['Pclass','Sex', 'Age','SibSp','Parch','Fare']   
+col_y = 'Survived'
+
+clf = DecisionTreeClassifier()
+clf.fit(data_train[cols_x], data_train[col_y])
+
+occupancy_predicted = clf.predict(data_test[cols_x])
+
+data_test['value'] = occupancy_predicted
+data_test.to_csv('base_solution.csv')
+
+print(data_test.head())
